925e7df1271929fe0fdc3ae9fe652eb763578fa8
Stefan Olivier <s.olivier1194@gmail.com>
2026-02-21 20:40:38 +0200

    chore: decrease hero card vertical margins

--- website/resources/js/Pages/Homepage.svelte
+++ website/resources/js/Pages/Homepage.svelte
@@ -82,7 +82,7 @@ $effect(() => {
 <div class="min-h-screen bg-bg flex flex-col p-6 md:p-8 lg:p-12">
     <main class="flex-1 flex flex-col items-center justify-center">
         <div class="flex flex-col items-center text-center justify-center mb-12">
-            <div class="flex flex-col sm:flex-row items-center gap-6 sm:gap-8 lg:gap-10 mb-24 md:mb-48">
+            <div class="flex flex-col sm:flex-row items-center gap-6 sm:gap-8 lg:gap-10 my-12 md:my-24">
                 <div class="w-24 h-24 sm:w-28 sm:h-28 rounded-full overflow-hidden bg-border flex-shrink-0 ring-2 ring-border">
                     <img
                         src="https://stefanolivier.imgix.net/img/owlsh.jpg"
