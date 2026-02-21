79b230446c50f5db8003ca278811bde0e92d4e26
Stefan Olivier <s.olivier1194@gmail.com>
2026-02-21 19:50:33 +0200

    feat: create scroll lock helper

---
+++ website/resources/js/Lib/scrollLock.ts
@@ -0,0 +1,34 @@
+let scrollLockCount = 0;
+let originalOverflow = '';
+let originalPaddingRight = '';
+
+function getScrollbarWidth(): number {
+    return window.innerWidth - document.documentElement.clientWidth;
+}
+
+export function lockScroll(): void {
+    if (scrollLockCount === 0) {
+        originalOverflow = document.body.style.overflow;
+        originalPaddingRight = document.body.style.paddingRight;
+        
+        const scrollbarWidth = getScrollbarWidth();
+        document.body.style.overflow = 'hidden';
+        if (scrollbarWidth > 0) {
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
+        }
+    }
+    scrollLockCount++;
+}
+
+export function unlockScroll(): void {
+    scrollLockCount--;
+    if (scrollLockCount <= 0) {
+        scrollLockCount = 0;
+        document.body.style.overflow = originalOverflow;
+        document.body.style.paddingRight = originalPaddingRight;
+    }
+}
+
+export function isScrollLocked(): boolean {
+    return scrollLockCount > 0;
+}
