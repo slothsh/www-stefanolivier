9bd720d2327e11c865fa8ae3adad2afed31930a4
Stefan Olivier <s.olivier1194@gmail.com>
2026-02-22 14:19:04 +0200

    feat: add model for cv content

---
+++ website/app/Models/CvContent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CvContent extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cv_content';
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'content' => 'object',
+        'tags' => CommaListToArray::class,
+    ];
+}
