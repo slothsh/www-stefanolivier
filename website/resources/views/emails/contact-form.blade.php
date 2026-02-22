c080f32f6fcba896fe616ba6b44cab72ce4e7a37
Stefan Olivier <s.olivier1194@gmail.com>
2026-02-22 12:13:30 +0200

    chore: format contact email template

--- website/resources/views/emails/contact-form.blade.php
+++ website/resources/views/emails/contact-form.blade.php
@@ -1,14 +1,14 @@
 <x-mail::message>
-# New Contact Form Submission
+    # New Contact Form Submission
 
-<x-mail::panel>
-**Name:** {{ $name }}<br>
-**Email:** {{ $email }}<br>
-**Message:**<br>
-{{ $message }}
-</x-mail::panel>
+    <x-mail::panel>
+        **Name:** {{ $name }}<br>
+        **Email:** {{ $email }}<br>
+        **Message:**<br>
+        {{ $message }}
+    </x-mail::panel>
 
-<x-mail::button :url="config('app.url')">
-Visit Website
-</x-mail::button>
+    <x-mail::button :url="config('app.url')">
+        Visit Website
+    </x-mail::button>
 </x-mail::message>
